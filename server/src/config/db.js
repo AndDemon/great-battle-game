83caef19118275dbb88643ff4af3b7b8f53166d1
Hrysenko Andrii <Andrii.Hrysenko@cs.khpi.edu.ua>
2026-05-19 21:23:47 +0300

    Update db.js

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: '06663441732006',
+    password: 'securepass',
     database: 'great_battle',
     waitForConnections: true,
     connectionLimit: 10,
